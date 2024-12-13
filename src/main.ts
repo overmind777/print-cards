88c5f64519ab4de8b07f46ded9c9c770096e86e2
Oleksandr Hembaruk <overmind@Mac-mini-Oleksandr.local>
2024-12-13 22:30:36 +0200

    add modal

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 function printForm() {
     const printArea = document.getElementById('container');
-    console.log('print-area: ', printArea)
+    console.log(printArea);
     if (!printArea) return;
 
     const originalContent = document.body.innerHTML;
     document.body.innerHTML = printArea.outerHTML;
     window.print();
     document.body.innerHTML = originalContent;
-    console.log('print');
 }
 
 // Додаємо до глобального контексту
